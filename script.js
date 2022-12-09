c7ba16eec740b41835737c71db4e361952d1793a
Muradova Farangiz [FE16] <102415848+farangiztae@users.noreply.github.com>
2022-12-09 15:07:54 +0500

    Update script.js

--- script.js
+++ script.js
@@ -17,19 +17,7 @@
 //sqrt(16, 2)  4
 //sqrt(8, 3)  2
 
-
-//1
-// function pow(x, n){
-//     x = 2;
-//     n = 3;
-//     alert(Math.pow(x, n));
-// }
-
-//2 
-// let num = 2.7324234;
-// console.log(Math.floor(num*1)/1);
-
-//3// 1. 
+// 1. 
 // let x = 2
 // let n = 3
 // function pow() {
@@ -38,6 +26,9 @@
 // pow()
 // console.log(pow(x, n))
 
+//2 
+// let num = 2.7324234;
+// console.log(Math.floor(num*1)/1);
 
 // 5. Math.sqrt(16, 2) // 4 (8, 3) // 2
 // let a = 16
