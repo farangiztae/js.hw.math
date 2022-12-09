0da0539a105c4c331333687c75d6c9ed82f0b909
Muradova Farangiz [FE16] <102415848+farangiztae@users.noreply.github.com>
2022-12-09 15:05:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+//HOMEWORK
+
+// 1 - function pow(x, n)
+//pow(2, 3) 8
+
+//2 - function floor(n)
+//floor(2.7324234) 2
+
+//3 - function ceil(n)
+//ceil(2.7)  3
+
+//4 - function round(n)
+//round(2.7)  3
+//round(2.7)  2
+
+//5 - function sqrt(x, n)
+//sqrt(16, 2)  4
+//sqrt(8, 3)  2
+
+
+//1
+// function pow(x, n){
+//     x = 2;
+//     n = 3;
+//     alert(Math.pow(x, n));
+// }
+
+//2 
+// let num = 2.7324234;
+// console.log(Math.floor(num*1)/1);
+
+//3// 1. 
+// let x = 2
+// let n = 3
+// function pow() {
+//     return x  n
+// }
+// pow()
+// console.log(pow(x, n))
+
+
+// 5. Math.sqrt(16, 2) // 4 (8, 3) // 2
+// let a = 16
+// let b = 2
+// function sqrt() {
+//     return a  (1/b);
+// }
+// sqrt()
+// console.log(sqrt(a, b));
